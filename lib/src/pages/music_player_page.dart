5bc1dd94646c164cf36541f1581b75497cc98fba
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-04-25 20:58:25 -0400

    added button and progress container

---
+++ lib/src/pages/music_player_page.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:musicplayer/src/widgets/custom_appbar.dart';
+
+class MusicPlayerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          CustomAppBar(),
+          ImagenDiscoDuracion(),
+          TituloPlay(),
+        ],
+      ),
+    );
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 40.0,
+      ),
+      margin: EdgeInsets.only(
+        top: 40.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Text(
+                'Far Away',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white.withOpacity(
+                    0.8,
+                  ),
+                ),
+              ),
+              Text(
+                '-Breaking Benjamin-',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.white.withOpacity(
+                    0.5,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          FloatingActionButton(
+            elevation: 0.0,
+            highlightElevation: 0.0,
+            backgroundColor: Color(0xFFF8CB51),
+            onPressed: () {},
+            child: Icon(
+              Icons.play_arrow,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 30.0,
+      ),
+      margin: EdgeInsets.only(
+        top: 100.0,
+      ),
+      child: Row(
+        children: <Widget>[
+          ImagenDisco(),
+          SizedBox(
+            width: 40.0,
+          ),
+          BarraProgreso(),
+          SizedBox(
+            width: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgreso extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(
+      color: Colors.white.withOpacity(
+        0.4,
+      ),
+    );
+    return Container(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            '00:00',
+            style: estilo,
+          ),
+          Stack(
+            children: <Widget>[
+              Container(
+                width: 3.0,
+                height: 230.0,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(
+                    0.1,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0.0,
+                child: Container(
+                  width: 3.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(
+                      0.8,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            '00:00',
+            style: estilo,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(
+        20.0,
+      ),
+      width: 250.0,
+      height: 250.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          200.0,
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [
+            Color(0xFF484750),
+            Color(0xFF1E1C24),
+          ],
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(
+          200.0,
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Image(
+              image: AssetImage(
+                'assets/aurora.jpg',
+              ),
+            ),
+            Container(
+              width: 25.0,
+              height: 25.0,
+              decoration: BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.circular(
+                  100.0,
+                ),
+              ),
+            ),
+            Container(
+              width: 18.0,
+              height: 18.0,
+              decoration: BoxDecoration(
+                color: Color(0xFF1C1C25),
+                borderRadius: BorderRadius.circular(
+                  100.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
